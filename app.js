bfe3b33799e3416fd99cc04d1b95f65f67a30582
arshati <am2089@hw.ac.uk>
2021-11-16 10:22:34 +0400

    updated

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express'); //creating app
 const app = express(); //make the app listen on port
 //send an HTTP response when receiving HTTP GET /
-app.get('/', (req, res) => {
-    res.send("Hello, your server is up and running"); });
+app.get('/', (req, res) => { 
+    res.sendFile('public/index.html', { root: __dirname });
+});
 const port = process.argv[2] || process.env.PORT || 3000; const server = app.listen(port, () => {
 console.log(`Cart app listening at http://localhost:${port}`); 
 });
